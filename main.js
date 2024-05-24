65e91c5158fa19478ebf247255986c380368de49
JBQ1 JBQ1 <quizkid91102@gmail.com>
2024-05-24 15:08:33 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+urlFig1 = 'https://www.artic.edu/artworks/129884/starry-night-and-the-astronauts'
+urlFig2 = 'https://www.artic.edu/artworks/28560/the-bedroom'
+urlFig3 = 'https://www.artic.edu/artworks/21023/buddha-shakyamuni-seated-in-meditation-dhyanamudra'
+
+
+function openPopup1() {
+    let popup = window.open(urlFig1, "Popup Window", "width=500, height=500");
+}
+
+function openPopup2() {
+    let popup = window.open(urlFig2, "Popup Window", "width=500, height=500");
+}
+
+function openPopup3() {
+    let popup = window.open(urlFig3, "Popup Window", "width=500, height=500");
+}
+
